ef4826c7ef48f879482c54c257d5286dd928170a
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-11-28 17:34:28 +0000

    16

---
+++ .release-it.js
@@ -0,0 +1,22 @@
+module.exports = {
+  git: {
+    // eslint-disable-next-line no-template-curly-in-string
+    commitMessage: 'release: v${version}',
+    requireBranch: 'ci/nightly-prereleases',
+    requireUpstream: false,
+    // eslint-disable-next-line no-template-curly-in-string
+    tagName: 'v${version}',
+  },
+  github: {
+    release: true,
+    draft: true,
+  },
+  npm: {
+    publish: false,
+  },
+  hooks: {
+    'after:bump': 'npm run build',
+    // eslint-disable-next-line no-template-curly-in-string
+    'before:git:release': 'git switch -c release-bot/${version}',
+  },
+}
