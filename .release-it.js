b585fb58302d9e927d85b6333968ae03cbab0588
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-12-01 15:57:30 +0000

    32

--- .release-it.js
+++ .release-it.js
@@ -15,7 +15,6 @@ module.exports = {
     publish: false,
   },
   hooks: {
-    'after:bump': 'npm run build',
     // eslint-disable-next-line no-template-curly-in-string
     'before:git:release': 'git switch -c release-bot/${version}'
   },
