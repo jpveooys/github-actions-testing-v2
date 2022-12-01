2f361cc1d82c5a027b4ff478242190bd84574a5c
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-12-01 16:45:07 +0000

    35

--- .release-it.js
+++ .release-it.js
@@ -2,7 +2,7 @@ module.exports = {
   git: {
     // eslint-disable-next-line no-template-curly-in-string
     commitMessage: 'release: v${version}',
-    pushArgs: '--follow-tags --set-upstream',
+    pushArgs: '--set-upstream',
     requireBranch: 'main',
     // eslint-disable-next-line no-template-curly-in-string
     tagName: 'v${version}',
