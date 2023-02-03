9eca01da633ae0d5c9c2f8d0e02eca7f2aacc508
jpveooys <66470099+jpveooys@users.noreply.github.com>
2023-02-03 15:58:37 +0000

    R2

--- .release-it.js
+++ .release-it.js
@@ -1,20 +1,13 @@
 module.exports = {
   git: {
-    // eslint-disable-next-line no-template-curly-in-string
-    commitMessage: 'release: v${version}',
-    pushArgs: '--set-upstream',
     requireBranch: 'main',
     // eslint-disable-next-line no-template-curly-in-string
     tagName: 'v${version}',
   },
   github: {
-    releaseName: 'v${version}'
-  },
-  npm: {
-    publish: false,
-  },
-  hooks: {
+    release: true,
     // eslint-disable-next-line no-template-curly-in-string
-    'before:git:release': 'git switch -c release-bot/${version}'
+    releaseName: 'v${version}',
   },
+  npm: false,
 }
